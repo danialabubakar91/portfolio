7a4adfc7ec1634ce2d24406f4697f90326ee7462
danial_ab <danialabubakar91@gmail.com>
2024-06-25 00:21:16 +0800

    Added base typography and fixed css hierarchy

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,12 @@
+const Home: React.FC = () => {
+  return (
+    <>
+      <h1>This is the homepage</h1>
+      <h2>homepage h2</h2>
+      <h3>homepage h3</h3>
+      <p>homepage paragraph</p>
+    </>
+  );
+};
 
-
-const Home:React.FC = () => {
-    return(
-        <h1>This is the homepage</h1>
-    )
-}
-
-export default Home
+export default Home;

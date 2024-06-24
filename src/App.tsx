27cb10f5571556ad469358a0d64cab4c51351ca6
danial_ab <danialabubakar91@gmail.com>
2024-06-24 18:46:24 +0800

    Setup base portfolio component and css reset

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+const App : React.FC = () => {
   const [count, setCount] = useState(0);
-  const something = 123;
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
